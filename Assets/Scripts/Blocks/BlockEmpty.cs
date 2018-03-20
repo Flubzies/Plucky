de9ca155e2b92ea2204594372d64183b040cf237
Unknown <fjgamerz@gmail.com>
2018-03-20 11:58:50 +0000

    Post Processing PogChamp

--- Assets/Scripts/Blocks/BlockEmpty.cs
+++ Assets/Scripts/Blocks/BlockEmpty.cs
@@ -2,5 +2,24 @@
 
 public class BlockEmpty : Block
 {
-	
+    void Start()
+    {
+        int x = Random.Range(0, 3);
+        Vector3 vec = Vector3.forward;
+        
+        switch(x)
+        {
+            case 0:
+            vec = Vector3.up;
+            break;
+            case 1: 
+            vec = Vector3.left;
+            break;
+            case 2:
+            vec = Vector3.forward;
+            break;
+        }
+        
+        transform.rotation = Quaternion.AngleAxis(x * 90, vec);
+    }
 }
