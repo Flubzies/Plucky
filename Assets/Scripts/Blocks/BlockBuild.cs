fb67ef70dec9a1e44cfb93e5242e3832b70b593c
Flubz <FarjadM96@gmail.com>
2018-10-12 01:39:06 +0100

    RemovedUneededStuff

--- Assets/Scripts/Blocks/BlockBuild.cs
+++ Assets/Scripts/Blocks/BlockBuild.cs
@@ -9,24 +9,3 @@ namespace BlockClasses
 		
 	}
 }
-// *Might* use this if I decide to have multiple blocks placeable at the same time.
-
-// List<IPlaceable> pList = new List<IPlaceable> ();
-
-// public List<IPlaceable> GetConnectedBlocks ()
-// {
-// 	_colliders = GetAdjacentSpaces (transform);
-// 	pList.Clear ();
-
-// 	for (int i = 0; i < _colliders.Count; i++)
-// 	{
-// 		Collider[] cols = Physics.OverlapSphere (_colliders[i], 0.2f, _blocksLM);
-// 		if (cols.Length != 0)
-// 		{
-// 			IPlaceable temp = cols[0].transform.GetComponent<IPlaceable> ();
-// 			if (temp != null) pList.Add (temp);
-// 		}
-// 	}
-
-// 	return pList;
-// }
